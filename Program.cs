5146f721fb751d596437e9aee681d2a6170a7be1
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2020-09-11 21:36:50 -0300

    Create project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace DotCOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
